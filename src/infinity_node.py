35588f911f552e0c5083d2938409d1dd2c9e54ae
kirkBoyd <kirkland.boyd@trincoll.edu>
2019-03-09 01:30:17 +0000

    complete working infinity_node logicgit add -A

--- src/infinity_node.py
+++ src/infinity_node.py
@@ -1,29 +1,34 @@
 #! /usr/bin/env python
 
 import rospy
-# from time import sleep
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 vel = Twist()
-vel.linear.x = 1
+vel.linear.x = 0.5
 vel.linear.y = 0
 vel.linear.z = 0
 vel.angular.x = 0
 vel.angular.y = 0
-vel.angular.z = 0
-global turn
+vel.angular.z = -0.25
 turn = False
 
 
 def callback(msg):
+    global turn
     x = msg.pose.pose.position.x
+    print("x")
+    print(x)
     y = msg.pose.pose.position.y
-    if x == 0.0 & y == 0.0:
-        turn != turn
-    if turn:
-        vel.angular.z = 0.5
-    else:
-        vel.angular.z = -0.5
+    print("y")
+    print(y)
+    if (-0.02 < x and x < 0.015) and (-0.01 < y and y < 0.003):
+        turn = not turn
+        if turn:
+            vel.angular.z = 0.25
+            vel.linear.x = 0.5
+        else:
+            vel.angular.z = -0.25
+            vel.linear.x = 0.5
 
 
 rospy.init_node('infinity_node')
