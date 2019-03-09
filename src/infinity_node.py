7ac20d886d30478d5ab51b87cce354768495ad66
kirkBoyd <kirkland.boyd@trincoll.edu>
2019-03-09 00:32:00 +0000

    fix style and syntax errors with infinity_node.py

--- src/infinity_node.py
+++ src/infinity_node.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
 import rospy
-#from time import sleep
-from geometry_msgs.msg import Twist 
+# from time import sleep
+from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 vel = Twist()
 vel.linear.x = 1
@@ -11,18 +11,21 @@ vel.linear.z = 0
 vel.angular.x = 0
 vel.angular.y = 0
 vel.angular.z = 0
-global turn 
+global turn
+turn = False
+
 
 def callback(msg):
     x = msg.pose.pose.position.x
     y = msg.pose.pose.position.y
-    if x==0.0 & y==0.0:
+    if x == 0.0 & y == 0.0:
         turn != turn
     if turn:
         vel.angular.z = 0.5
     else:
-        vel.angular.z = -0.5     
-            
+        vel.angular.z = -0.5
+
+
 rospy.init_node('infinity_node')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 sub = rospy.Subscriber('/odom', Odometry, callback)
